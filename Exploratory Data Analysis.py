6433daa073c54c49fc24859f4a6d083dc80a15bc
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-10-12 12:26:07 -0500

    Update Exploratory Data Analysis.py

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -280,8 +280,9 @@ df['cum_perc'] = 100*df.cum_sum/df.val1.sum()
 # Replace Values of a variable
 Char_Codes = {"Char_Var1": {"Value1": New_Vaue1, "Value2": New_Vaue2},
               "Char_Var2": {"Value1": New_Vaue1, "Value2": New_Vaue2, "Value3": New_Vaue3, "Value4": New_Vaue4 }}
+Df.replace(Char_Codes, inplace=True)
 
-
+# 
 
 
 
