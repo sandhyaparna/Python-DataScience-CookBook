b64335aa7a801866597bcbf527e5aa018e4bc386
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-10-24 15:21:22 -0500

    Update Pre-Processing.py

--- Natural Language Processing/Pre-Processing.py
+++ Natural Language Processing/Pre-Processing.py
@@ -5,6 +5,8 @@ from nltk import *
 # 1.Removing stop words
 from nltk.corpus import stopwords
 nltk.download('stopwords')
+nltk.download('punkt')
+
 stop_words = set(stopwords.words('english'))  # stop_words = nltk.corpus.stopwords.words('english')
 new_stop_words = ['stopWord1','stopWord2']
 stop_words.extend(new_stop_words)
@@ -12,6 +14,7 @@ stop_words.extend(new_stop_words)
 Df["Text_Var2"] = Df["Text_Var"].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop_words)]))
 # Convert text data into small letter and split into words - words are seperated by comas
 # Removes stop words from the Text var - Text_Var1 has words seperated by comas
- Df["Text_Var1"] = Df["Text_Var"].str.lower().str.split()
+ Df["Text_Var1"] = Df["Text_Var"].str.lower().str.split() -- Same as Tokenization
  Df["Text_Var1"] = Df["Text_Var1"].apply(lambda x: [item for item in x if item not in stop_words])
-
+# Word Tokenization - words are seperated by commas
+Df["Text_Var3"] = Df["Text_Var"].apply(nltk.word_tokenize)
