492a3380be666f177d0b83e2057f65528e1e06da
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-05 15:37:26 -0600

    Create Hypothesis Tests.py

---
+++ Hypothesis Tests.py
@@ -0,0 +1,14 @@
+https://machinelearningmastery.com/statistical-hypothesis-tests-in-python-cheat-sheet/
+
+### Normality Tests
+##### Shapiro-Wilk Test - https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.shapiro.html
+
+##### D’Agostino’s K^2 Test
+
+##### Anderson-Darling Test
+
+### Correlation Tests
+##### Pearson’s Correlation Coefficient
+
+##### 
+##### 
