ef7c23461ac645ec5d2388922fb76ab3cb4c3dc7
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-07 16:54:29 -0600

    Update Regression.py

--- Regression.py
+++ Regression.py
@@ -1,1 +1,8 @@
-In R- plot(model_name) returns 4 plots
+pd.options.display.max_rows = 1500
+pd.set_option('display.max_columns', None)  # or 1000
+pd.set_option('display.max_rows', None)  # or 1000
+pd.set_option('display.max_colwidth', -1)  # or 199
+
+
+
+
