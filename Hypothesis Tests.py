8a8ec6e82bcdb5af294ba389df7f1f4a7dc2e3e0
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-10 16:35:03 -0600

    Update Hypothesis Tests.py

--- Hypothesis Tests.py
+++ Hypothesis Tests.py
@@ -2,21 +2,16 @@ https://machinelearningmastery.com/statistical-hypothesis-tests-in-python-cheat-
 
 ### Normality Tests
 ##### Histogram
-##### Q-Q Plot
-y_norm_residuals = sm_OLS_1.get_influence().resid_studentized_internal  # normalized residuals
+In repository - Python-DataScience-CookBook/Exploratory Data Analysis.py
+import seaborn as sns
+sns.distplot(Df.Var.dropna())
 
-from statsmodels.graphics.gofplots import ProbPlot
-QQ = ProbPlot(y_norm_residuals)
-plot_QQ = QQ.qqplot(line='45', alpha=0.5, color='#4C72B0', lw=1)
-plot_QQ.set_figheight(8)
-plot_QQ.set_figwidth(12)
-plot_QQ.axes[0].set_title('Normal Q-Q')
-plot_QQ.axes[0].set_xlabel('Theoretical Quantiles')
-plot_QQ.axes[0].set_ylabel('Standardized Residuals');
-abs_norm_resid = np.flip(np.argsort(np.abs(y_norm_residuals)), 0)
-abs_norm_resid_top_3 = abs_norm_resid[:3]
-for r, i in enumerate(abs_norm_resid_top_3):
-    plot_QQ.axes[0].annotate(i, xy=(np.flip(QQ.theoretical_quantiles, 0)[r], y_norm_residuals[i]));
+##### Q-Q Plot
+import numpy as np 
+import pylab 
+import scipy.stats as stats
+stats.probplot(Df.Var, dist="norm", plot=pylab)
+pylab.show()
 
 ##### Shapiro-Wilk Test - https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.shapiro.html
 
