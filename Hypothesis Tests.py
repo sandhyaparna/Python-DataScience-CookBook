8d3bb0fa140706cbb8bf5ed0a271e5585f6d939f
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-10 16:55:29 -0600

    Update Hypothesis Tests.py

--- Hypothesis Tests.py
+++ Hypothesis Tests.py
@@ -13,11 +13,18 @@ import scipy.stats as stats
 stats.probplot(Df.Var, dist="norm", plot=pylab)
 pylab.show()
 
+##### Normal Test
+k2, p = stats.normaltest(Energy.x)
+
 ##### Shapiro-Wilk Test - https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.shapiro.html
+from scipy import stats
+w,p = stats.shapiro(Df.x)
 
 ##### D’Agostino’s K^2 Test
+stats.kstest(Df.x,'norm')
 
 ##### Anderson-Darling Test
+stats.anderson((Df.x,'norm')
 
 ### Correlation Tests
 ##### Pearson’s Correlation Coefficient
