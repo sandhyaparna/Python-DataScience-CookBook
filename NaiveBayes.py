0b8312e4d3b8d15bed038b1510e888f1bcb690a2
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-13 14:29:22 -0600

    Create NaiveBayes.py

---
+++ NaiveBayes.py
@@ -0,0 +1,13 @@
+pd.options.display.max_rows = 1500
+pd.set_option('display.max_columns', None)  # or 1000
+pd.set_option('display.max_rows', None)  # or 1000
+pd.set_option('display.max_colwidth', -1)  # or 199
+
+import sklearn as sklearn
+from sklearn import *
+from sklearn.naive_bayes import *
+
+### Gaussian Naive Bayes ###
+
+
+
