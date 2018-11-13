e9b0c5d6140f075320b5ca885cdb7e92da687d86
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-13 14:22:34 -0600

    Update Statistics.py

--- Statistics.py
+++ Statistics.py
@@ -2,5 +2,12 @@ https://docs.scipy.org/doc/scipy/reference/stats.html
 
 # Data Transformation using Box-Cox
 from scipy import stats
-Bank['age_trans'] = stats.boxcox(Bank['age'],alpha=0.05)[0]
+Df['Var_trans'] = stats.boxcox(Df['Var'])[0]
+
+stats.boxcox(Df['Var']) # Produces array of 2 values. 1st value is the transformed data, 2nd value is lmbda found to fit the data to normal
+stats.boxcox(Df['Var'],lmbda=value) # Produces array with only 1 value - Transformed data values
+stats.boxcox(Df['Var'],alpha=value between 0 & 1) # Produces array with 3 values - Transformed data values, lambda value, (1-alpha)% confidence intervals for lmbda
+
+
+
 
