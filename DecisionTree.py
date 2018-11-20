4c1e08b142413177ce1593c3010a8c40de661449
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-20 17:51:38 -0600

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -2,10 +2,10 @@
 # CANNOT handle categorical data - Should be encoded using different encoding techniques
 # Target Variable can be BinaryLabel/MultiLabel and Numeric/Character
 
-# For MultiLabel
-# cross_val_score function shouldn't have scoring parameter
-# cross_val_predict function should have method='predict' & not method='predict_proba'
+# scoring="roc_auc" works only when Target Var is BinaryNumeric
+# But If no 'scoring' parameter is mentioned, it produces roc-auc by default for any type of Target var
 
+# cross_val_predict function should have method='predict' & not method='predict_proba'
 # method='predict_proba' - Generates array with 2 columns for Binary, 3 columns for Multi (columns correspond to the classes in sorted order)
 # method='predict' - Generates array with column and different predicted categories(Original Categories as inputed for training)
 
