c6d39f097fb8916cc4bdbd4133738427760420dd
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-20 17:37:02 -0600

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,5 +1,8 @@
 # CANNOT handle MISSING values
 # CANNOT handle categorical data - Should be encoded using different encoding techniques
+# Target Variable can be BinaryLabel/MultiLabel and Numeric/Character
+
+
 
 
 
