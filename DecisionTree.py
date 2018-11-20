81c6333006ce2859fa3aac37092152bb8960ec14
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-20 17:44:44 -0600

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -6,7 +6,7 @@
 # cross_val_score function shouldn't have scoring parameter
 # cross_val_predict function should have method='predict' & not method='predict_proba'
 
-# method='predict_proba' - Generates array with 2 columns for Binary, 3 columns for Multi
+# method='predict_proba' - Generates array with 2 columns for Binary, 3 columns for Multi (columns correspond to the classes in sorted order)
 # method='predict' - Generates array with column and different predicted categories(Original Categories as inputed for training)
 
 
