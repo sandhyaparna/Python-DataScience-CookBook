4491239312033101a67de266242ac9a22aeab147
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-11-21 09:33:26 -0600

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -42,19 +42,35 @@ y_Pred = np.where(y_Pred>Confidence_value,1,0)
 # Applying cross_val_score doesn't fit the DecisionTreeModel using the given Independent & Dependent data
 # We need to fit the model
 DecisionTreeModel = DecisionTreeModel.fit(X,y)
+
+
 # Only after the DecisionTreeModel is fit using the data - attributes & Methods in the below link can be applied
 https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html#sklearn.tree.DecisionTreeClassifier
  
+DecisionTreeModel.feature_importances_ # Produces an imp value/factor for each variable in the input data
+DecisionTreeModel.tree_ #<sklearn.tree._tree.Tree at 0x1af4e920> ???
 
 DecisionTreeModel.apply(X)  #Gives the number of the node in the Decision tree where a particular OBSERVATION ended
-DecisionTreeModel.decision_path(X) #Doesn't seem to work, checkout later
+DecisionTreeModel.decision_path(X) #Doesn't seem to work, checkout later ???
 DecisionTreeModel.get_params() #Get parameters for fitted estimator
 DecisionTreeModel.predict(Test_X) #Predict on the test data
 DecisionTreeModel.predict_proba(Test_X) #Predict on the test data - Gives array with no of columns=no of labels in Target
 DecisionTreeModel.predict_log_proba(Test_X) #Predict on the test data - Gives array with no of columns=no of labels in Target
 DecisionTreeModel.score(Test_X,Test_y) # Mean Accuracy of Test Data
 
-
+# Decision Tree Viz- Works when Target Var is both Cat/Num
+# http://webgraphviz.com/
+# Use the code from the saved dot file in the above link - it gives viz
+data_feature_names = list(X)
+target_names = pd.unique(y)
+# Convert data type
+target_names = target_names.astype('<U10') # Works when Target Var is both Cat/Num
+# Viz code to a file - paste code in http://webgraphviz.com/
+# DecisionTreeModel is one that is fitted on X,y
+dotfile = open("path/DecisionTreeModel_Tree.dot", 'w')
+tree.export_graphviz(DecisionTreeModel, out_file=dotfile, feature_names=data_feature_names,
+                     class_names=target_names,filled=True, rounded=True)
+dotfile.close()
 
 
     
