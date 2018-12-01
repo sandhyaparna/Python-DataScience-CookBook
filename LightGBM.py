3d983773a2d7c40565b8c6c4812752c8b9c95125
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-01 00:02:30 -0600

    Create LightGBM.py

---
+++ LightGBM.py
@@ -0,0 +1,7 @@
+# Parameter Tuning
+# https://www.analyticsvidhya.com/blog/2017/06/which-algorithm-takes-the-crown-light-gbm-vs-xgboost/
+# https://medium.com/@pushkarmandot/https-medium-com-pushkarmandot-what-is-lightgbm-how-to-implement-it-how-to-fine-tune-the-parameters-60347819b7fc
+
+
+
+
