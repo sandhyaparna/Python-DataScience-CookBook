464ae6fe27b814d16fa1ba70f4c7c4c94991f7af
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-01 13:08:40 -0600

    Update GBM.py

--- GBM.py
+++ GBM.py
@@ -1,13 +1,14 @@
-pd.options.display.max_rows = 1500
-pd.set_option('display.max_columns', None)  # or 1000
-pd.set_option('display.max_rows', None)  # or 1000
-pd.set_option('display.max_colwidth', -1)  # or 199
+# CANNOT handle MISSING values
+# CANNOT handle categorical data - Should be encoded using different encoding techniques
+# Target Variable can be BinaryLabel/MultiLabel and Numeric/Character
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingClassifier.html
 
 # Parameter Tuning
 # https://www.analyticsvidhya.com/blog/2016/02/complete-guide-parameter-tuning-gradient-boosting-gbm-python/
 # https://medium.com/all-things-ai/in-depth-parameter-tuning-for-gradient-boosting-3363992e9bae
 
-## Shrinkage parameter - Vary shrinkage parameter(all other parameters are default) to check for Test AUC, choose shrinkage parameter 
+## learning_rate/Shrinkage parameter - Vary shrinkage parameter(all other parameters are default) to check for Test AUC, choose shrinkage parameter 
 # that gives highest Test AUC and then vary number of trees to avoid over-fitting. Optimal values usually lie between 0.01-0.2
 
 ## sub-samples - The fraction of samples to be used for fitting the individual base learners. Selection is done by random sampling. 
@@ -16,18 +17,23 @@ pd.set_option('display.max_colwidth', -1)  # or 199
 # Choosing subsample < 1.0 leads to a reduction of variance and an increase in bias.
 # Typical values ~0.8 generally work fine but can be fine-tuned further. Values between 0.5-0.8 leads to good results for small and moderate sized training sets.
 
+## loss -  ‘exponential’ performs AdaBoost algorithm
 
+## random_state is used to make outcomes consistent across different runs using the same parameters
 
 gblinear booster treats missing values as zeros???
 
+GBMModel = GradientBoostingClassifier()
+GBMModel = GBMModel.fit(X,y)
 
 # Plot of AUC vs Learning Rates (2 curves - 1 for Training and another for Test)
 # Every single time this below code is run, for same Train and Test datasets - the AUC values changes for a particular learning rate
+# Setting random_state to a fixed value avoids this problem 
 learning_rates = [1, 0.5, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05, 0.025, 0.01]
 train_results = []
 test_results = []
 for eta in learning_rates:
-   model = GradientBoostingClassifier(learning_rate=eta)
+   model = GradientBoostingClassifier(learning_rate=eta,random_state=0)
    model.fit(x_train, y_train)
    train_pred = model.predict(x_train)
    false_positive_rate, true_positive_rate, thresholds = roc_curve(y_train, train_pred)
