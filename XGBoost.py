9fe278f6e83f5cdd4697a2558b30aad253b4af63
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-01 00:02:27 -0600

    Update XGBoost.py

--- XGBoost.py
+++ XGBoost.py
@@ -1,1 +1,15 @@
-Parameter Tuning - https://www.analyticsvidhya.com/blog/2016/03/complete-guide-parameter-tuning-xgboost-with-codes-python/
+# Parameter Tuning
+# https://www.analyticsvidhya.com/blog/2016/03/complete-guide-parameter-tuning-xgboost-with-codes-python/
+# Explains how to handle imbalnced datsets in xgboost - https://xgboost.readthedocs.io/en/latest/tutorials/param_tuning.html
+# https://xgboost.readthedocs.io/en/latest/parameter.html
+
+
+
+
+
+
+
+
+  
+  
+  
