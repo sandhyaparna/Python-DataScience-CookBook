41ad472176d96461c70dc254f3f93a01c4f830bc
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-02 00:04:40 -0600

    Update XGBoost.py

--- XGBoost.py
+++ XGBoost.py
@@ -1,9 +1,18 @@
+# CAN handle MISSING values
+# CANNOT handle categorical data - Should be encoded using different encoding techniques
+# Target Variable can be BinaryLabel/MultiLabel and Numeric/Character
+
 # Parameter Tuning
 # https://www.analyticsvidhya.com/blog/2016/03/complete-guide-parameter-tuning-xgboost-with-codes-python/
+# https://xgboost.readthedocs.io/en/latest/index.html
 # Explains how to handle imbalnced datsets in xgboost - https://xgboost.readthedocs.io/en/latest/tutorials/param_tuning.html
 # https://xgboost.readthedocs.io/en/latest/parameter.html
 
 
+import xgboost as xgb
+from xgb import *
+
+DMatrix
 
 
 
