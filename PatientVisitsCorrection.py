68a00ab9a45382261634a3ba56a137f80d2051b3
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-07 14:31:54 -0600

    Create PatientVisitsCorrection.py

---
+++ PatientVisitsCorrection.py
@@ -0,0 +1,89 @@
+#### MRN Sample Check code ####
+MRNSample_Visits1 =  pd.read_csv("C:/Users/spashikanti/Desktop/Readmissions - 2018/Py DataFrames/MRNSample_Visits1.csv",
+                 parse_dates = ['Arrival','Departure']) # To import variable in datetime format default format is '%d%b%Y:%H:%M:%S.%f')
+
+MRNSample_Visits1=MRNSample_Visits1[['Arrival','Departure']]
+ 
+# Sort data by Arrival,Departure
+MRNSample_Visits1 = MRNSample_Visits1.sort_values(by=['Arrival','Departure'])
+
+# Remove cases where Departure is before Arrival
+
+# Remove duplicate Arrivals - within each Arrival pick max Departure
+import pip
+from pandasql import *
+import pandasql as ps
+query = """select Arrival,max(Departure) as Departure from MRNSample_Visits1 group by Arrival"""
+MRNSample_Visits1 = ps.sqldf(query, locals())
+
+# Convert Arrival,Departure to datetime
+import datetime
+MRNSample_Visits1['Arrival'] =  pd.to_datetime(MRNSample_Visits1['Arrival'], format='%Y-%m-%d %H:%M:%S.%f')
+MRNSample_Visits1['Departure'] =  pd.to_datetime(MRNSample_Visits1['Departure'], format='%Y-%m-%d %H:%M:%S.%f')
+
+# Check if Next_Arrival is greater than Departure+2Days
+MRNSample_Visits1['Next_Arrival_Dep_Gre2'] = np.where((MRNSample_Visits1['Arrival'].shift(-1))>((MRNSample_Visits1['Departure']) + pd.Timedelta(days=2)),1,0)
+
+# Check if all values of Next_Arrival_Dep_Gre2 are 1except last obs within a MRN
+
+#### 1st Iteration,2nd,3
+
+# Create MRN 
+MRNSample_Visits1 = MRNSample_Visits1.assign(MRN=1)
+
+# Self join data using MRN
+MRNSample_Visits1 = MRNSample_Visits1[['MRN','Arrival','Departure']].merge(MRNSample_Visits1[['MRN','Arrival','Departure']], how='inner',on='MRN')
+
+# Since there are no duplicate Arrivals
+# Delete obs where Arrival_y<Arrival_x and not Arrival_y<=Arrival_x because id last patientID is unique to itself - it goes away
+# MRNSample_Visits1 = MRNSample_Visits1[(MRNSample_Visits1.Arrival_y>=MRNSample_Visits1.Arrival_x)]
+
+# Check if time difference between A_y & A_x is >=0 & A_y &D_x<=2days if yes then Ay=Ax and Dy is max(Dx,Dy)
+MRNSample_Visits1['Departure_New'] = np.where(( ((MRNSample_Visits1['Arrival_y'])<=((MRNSample_Visits1['Departure_x'])+(pd.Timedelta(days=2)))) & ((MRNSample_Visits1.Arrival_y)>=(MRNSample_Visits1.Arrival_x)) ), 
+                                                np.maximum(MRNSample_Visits1['Departure_x'],MRNSample_Visits1['Departure_y']),
+                                                MRNSample_Visits1['Departure_x'])
+
+MRNSample_Visits1['Departure_New1'] = np.where(( ((MRNSample_Visits1['Arrival_x'])<=((MRNSample_Visits1['Departure_y'])+(pd.Timedelta(days=2)))) & ((MRNSample_Visits1.Arrival_x)>=(MRNSample_Visits1.Arrival_y)) ), 
+                                                np.maximum(MRNSample_Visits1['Departure_x'],MRNSample_Visits1['Departure_y']),
+                                                MRNSample_Visits1['Departure_x'])
+
+# Extract Arrival_x and Departure_New
+MRNSample_Visits1a = MRNSample_Visits1[['Arrival_x','Departure_New']].drop_duplicates()
+MRNSample_Visits1a = MRNSample_Visits1a.rename(columns={"Departure_New":"Departure"})
+
+MRNSample_Visits1b = MRNSample_Visits1[['Arrival_x','Departure_New1']].drop_duplicates()
+MRNSample_Visits1b = MRNSample_Visits1b.rename(columns={"Departure_New1":"Departure"})
+
+MRNSample_Visits1 = pd.concat([MRNSample_Visits1a,MRNSample_Visits1b])
+
+# Remove duplicate Arrivals - within each Arrival pick max Departure
+import pip
+from pandasql import *
+import pandasql as ps
+query = """select Arrival_x as Arrival,max(Departure) as Departure from MRNSample_Visits1 group by Arrival_x"""
+MRNSample_Visits1 = ps.sqldf(query, locals())
+
+# Convert Arrival,Departure to datetime
+import datetime
+MRNSample_Visits1['Arrival'] =  pd.to_datetime(MRNSample_Visits1['Arrival'], format='%Y-%m-%d %H:%M:%S.%f')
+MRNSample_Visits1['Departure'] =  pd.to_datetime(MRNSample_Visits1['Departure'], format='%Y-%m-%d %H:%M:%S.%f')
+
+# Remove duplicate Arrivals - within each Departure pick min Arrival
+import pip
+from pandasql import *
+import pandasql as ps
+query = """select Departure,min(Arrival) as Arrival from MRNSample_Visits1 group by Departure"""
+MRNSample_Visits1 = ps.sqldf(query, locals()) 
+
+# Convert Arrival,Departure to datetime
+import datetime
+MRNSample_Visits1['Arrival'] =  pd.to_datetime(MRNSample_Visits1['Arrival'], format='%Y-%m-%d %H:%M:%S.%f')
+MRNSample_Visits1['Departure'] =  pd.to_datetime(MRNSample_Visits1['Departure'], format='%Y-%m-%d %H:%M:%S.%f')
+
+# Sort data by Arrival
+MRNSample_Visits1 = MRNSample_Visits1.sort_values(by=['Arrival'])
+MRNSample_Visits1 = MRNSample_Visits1 [['Arrival','Departure']]
+
+
+# Check if Next_Arrival is greater than Departure+2Days
+MRNSample_Visits1['Next_Arrival_Dep_Gre2'] = np.where((MRNSample_Visits1['Arrival'].shift(-1))>((MRNSample_Visits1['Departure']) + pd.Timedelta(days=2)),1,0)
