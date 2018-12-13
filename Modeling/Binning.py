0151a62879eb5d4d311428c6c2ae98fd1b1b54b0
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-12 20:36:26 -0600

    Update Binning.py

--- Modeling/Binning.py
+++ Modeling/Binning.py
@@ -9,11 +9,16 @@
 # Width of each bin = (Max-Min)/k
 Df['Var_Bin'] = pd.cut(Df['Num_Var'],k) 
 
+### User Defined Binning ###
+# DONT use square brackets while creating bins-even if (), it is actually equivalent to (]
+bins = pd.IntervalIndex.from_tuples([(10,20), (20, 30), (30,40), (40, 50),(50,60), (60, 70),(70,80), (80, 90)])
+Df['Var_Bin'] = pd.cut(Df['Num_Var'], bins)
+
 ### Equal Freq binning
 # https://pandas.pydata.org/pandas-docs/version/0.23.4/generated/pandas.qcut.html
 # q is number of quantiles or array of quantiles
 # q is 10 for deciles, 4 for quantiles etc; array of quantiles:[0, .25, .5, .75, 1.] for quartiles
-Df['Var_Bin'] = pd.cut(Df['Num_Var'],q) 
+Df['Var_Bin'] = pd.qcut(Df['Num_Var'],q) 
 
 ### Entropy Based Binning
 # http://www.saedsayad.com/unsupervised_binning.htm
