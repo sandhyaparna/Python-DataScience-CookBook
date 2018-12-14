a949584b8880caa11c99fcdaa571ab6b09b527b0
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-14 10:24:17 -0600

    Update Python Modules.py

---
+++ Modeling/Python Modules.py
@@ -0,0 +1,26 @@
+#### Run ####
+import sklearn as sklearn
+from sklearn import *
+from sklearn.tree import *
+from sklearn.ensemble import *
+from sklearn.linear_model import *
+from sklearn.neighbors import *
+from sklearn.svm import *
+from sklearn.naive_bayes import *
+
+from sklearn.preprocessing import *
+from sklearn.model_selection import *
+from sklearn.metrics import *
+
+# Imports related to Viz
+from sklearn.externals.six import StringIO  
+from IPython.display import Image  
+import pydotplus as pydotplus
+import graphviz
+
+# Support functions import
+from support_functions import calculate_accuracy, plot_confusion_matrix
+
+
+
+
