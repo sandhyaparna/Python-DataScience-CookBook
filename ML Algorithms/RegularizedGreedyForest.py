eaf0c3324399d8918a00da941b73d74af3bc3f9e
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-15 12:39:07 -0600

    Create RegularizedGreedyForest.py

---
+++ ML Algorithms/RegularizedGreedyForest.py
@@ -0,0 +1,9 @@
+# https://github.com/RGF-team/rgf
+# https://github.com/RGF-team/rgf/tree/master/python-package
+
+pip install rgf_python
+import rgf as rgf # Actually rgf_python
+
+# Above github link has code, parameter tuning
+
+
