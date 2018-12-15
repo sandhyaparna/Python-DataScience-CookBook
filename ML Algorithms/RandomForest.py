87147f0b8beb7acb2202784f9d3338bd8b97fa53
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-15 11:15:45 -0600

    Update RandomForest.py

--- ML Algorithms/RandomForest.py
+++ ML Algorithms/RandomForest.py
@@ -1,6 +1,6 @@
-
-
-
+# CANNOT handle MISSING values
+# CANNOT handle categorical data - Should be encoded using different encoding techniques
+# Target Variable can be BinaryLabel/MultiLabel and Numeric/Character
 
 # https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
 # https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestRegressor.html
