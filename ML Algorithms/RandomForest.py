1d44e67c0e3e619449f9c4b565fe7c2e8193a02f
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-15 12:11:10 -0600

    Update RandomForest.py

--- ML Algorithms/RandomForest.py
+++ ML Algorithms/RandomForest.py
@@ -10,6 +10,8 @@
 
 # Parameter Tuning
 # https://www.analyticsvidhya.com/blog/2015/06/tuning-random-forest-model/
+# https://medium.com/all-things-ai/in-depth-parameter-tuning-for-random-forest-d67bb7e920d
+# https://medium.com/all-things-ai/in-depth-parameter-tuning-for-gradient-boosting-3363992e9bae
 
 # max_features: Number of features allowed to try in individual tree.
 # Increasing max_features generally improves the performance of the model but may overfit
@@ -19,6 +21,8 @@
 # n_estimators: Number of trees in the forest.
 # Higher number of trees give you better performance but makes your code slower.
 # You should choose as high value as your processor can handle because this makes your predictions stronger and more stable.
+
+# min_samples_split: Min samples at internal node. Vary from 0.1 to 1 and see the graphs of Train & Test
 
 # min_sample_leaf: 
 # A smaller leaf makes the model more prone to capturing noise in train data. 
@@ -30,6 +34,12 @@
 
 # oob_score: This method simply tags every observation used in different tress. And then it finds out a maximum vote score for every 
 # observation based on only trees which did not use this particular observation to train itself.
+
+from sklearn.ensemble import *
+RandomForestModel = RandomForestClassifier(max_depth=4,random_state=0)
+RandomForestModel = RandomForestModel.fit(X,y)
+
+
 
 
 
