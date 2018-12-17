b659459145059c3d58473113ede1aae421facf07
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-16 21:12:47 -0600

    Create Overfitting.py

---
+++ Modeling/Overfitting.py
@@ -0,0 +1,25 @@
+# https://ai.googleblog.com/2015/08/the-reusable-holdout-preserving.html
+
+# Thresholdout
+from numpy import *
+def Thresholdout(sample, holdout, q):
+   # function q is what you’re “testing” - e.g., model loss
+   sample_mean = mean([q(x)  for x in sample])
+   holdout_mean = mean([q(x)  for x in holdout])
+   sigma = 1.0 / sqrt(len(sample))
+   threshold = 3.0*sigma
+   if (abs(sample_mean - holdout_mean)
+          < random.normal(threshold, sigma) ):
+       # q does not overfit: your “training estimate” is good
+       return sample_mean
+   else:
+       # q overfits (you may have overfit using your training data)
+       return holdout_mean + random.normal(0, sigma)
+
+
+
+
+
+       
+       
+       
