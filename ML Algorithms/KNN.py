1e7d5bd201465df52bf193d50a7759a6ab8515c7
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-18 15:52:30 -0600

    Update KNN.py

--- ML Algorithms/KNN.py
+++ ML Algorithms/KNN.py
@@ -1,1 +1,17 @@
+# https://www.analyticsvidhya.com/blog/2018/03/introduction-k-neighbours-algorithm-clustering/
+# https://kevinzakka.github.io/2016/07/13/k-nearest-neighbor/
 
+# Feature scaling
+from sklearn.preprocessing import StandardScaler  
+scaler = StandardScaler()  
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)  
+X_test = scaler.transform(X_test) 
+
+# Model
+from sklearn.neighbors import KNeighborsClassifier  
+classifier = KNeighborsClassifier(n_neighbors=5)  
+classifier.fit(X_train, y_train) 
+
+# To find optimal value of k
