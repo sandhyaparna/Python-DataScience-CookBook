82ae2e463257049b3a4a715469217002d52162f7
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2018-12-18 17:19:58 -0600

    Update KNN.py

--- ML Algorithms/KNN.py
+++ ML Algorithms/KNN.py
@@ -2,6 +2,8 @@
 # https://kevinzakka.github.io/2016/07/13/k-nearest-neighbor/
 # https://stackabuse.com/k-nearest-neighbors-algorithm-in-python-and-scikit-learn/
 
+from sklearn.neighbors import KNeighborsClassifier
+
 # Feature scaling
 from sklearn.preprocessing import StandardScaler  
 scaler = StandardScaler()  
@@ -16,3 +18,17 @@ classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(X_train, y_train) 
 
 # To find optimal value of k
+neighbors = list(range(1,50))
+# empty list that will hold cv scores
+cv_scores = []
+# perform 10-fold cross validation
+for k in myList:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    scores = cross_val_score(knn, X_train, y_train, cv=10, scoring='accuracy')
+    cv_scores.append(scores.mean())
+# changing to misclassification error
+MSE = [1 - x for x in cv_scores]
+
+
+
+
