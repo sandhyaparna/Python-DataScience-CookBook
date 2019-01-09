a69f1063820e92fc28bdadb5414381bfa9af3a5c
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-01-09 10:01:18 -0600

    Update Feature Understanding.py

--- Modeling/Feature Understanding.py
+++ Modeling/Feature Understanding.py
@@ -1,3 +1,4 @@
+# Delete noisy features - where Trend_Correlation<0.95 and where there are unequal number of trend changes
 from featexp import get_trend_stats
 stats = get_trend_stats(data=data_train, target_col='target', data_test=data_test)
 
