9183066b593b00daaa16ef326db6225513e117f9
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-01-22 14:06:54 -0600

    Create Viz.py

---
+++ DataVisualization/Viz.py
@@ -0,0 +1,9 @@
+### Chartify Module
+# https://github.com/spotify/chartify
+# https://labs.spotify.com/2018/11/15/introducing-chartify-easier-chart-creation-in-python-for-data-scientists/
+# https://chartify.io/
+
+
+
+
+
