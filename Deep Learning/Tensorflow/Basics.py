391aae9be5f67011a10402fdfd0b8f51bfafa819
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-02-19 10:47:00 -0600

    Create Basics.py

---
+++ Deep Learning/Tensorflow/Basics.py
@@ -0,0 +1,18 @@
+
+
+# 
+import tensorflow as tf
+from tf.keras.layers import *
+
+inputs = Inputs(n)
+hidden = Dense(d1)(inputs)
+outputs = Dense(2)(hidden)
+model = Model(inputs, outputs)
+
+
+
+
+
+
+
+
