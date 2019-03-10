c71b2b453ba1d8750e5b13563a42c08c0c2e7b0f
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-03-09 18:32:16 -0600

    Update CNN.py

--- Deep Learning/CNN.py
+++ Deep Learning/CNN.py
@@ -1,6 +1,24 @@
 # https://github.com/aamini/introtodeeplearning_labs/blob/master/lab2/Part1_mnist_solution.ipynb
 # 
 
+* CNN on Image
+  * Convolution is added on top of NN in model
+  * Filters generated in tf.keras.layers.Conv2D are not random. They start with a set of known good filters in a similar 
+    way to the pattern fitting that you saw earlier, and the ones that work from that set are learned over time.
+  * tf.keras.layers.MaxPooling2D(2,2) implies max value out of the 4 values of 2*2 matrix survives
 
+### CNN
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28, 28, 1)), #Generate 64 filters, each of 3*3 matrix, 
+                                                       # ReLU activation, 1 in input shape is where we are mentioning colour depth
+  tf.keras.layers.MaxPooling2D(2, 2),  # max value out of the 4 values of 2*2 matrix survives
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.summary()
 
 
