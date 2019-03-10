8cfec93c60e9a304ee4a32970dee3d341a10b48e
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-03-09 18:42:51 -0600

    Update DeepLearningAICourse.py

--- Deep Learning/DeepLearningAICourse.py
+++ Deep Learning/DeepLearningAICourse.py
@@ -14,8 +14,9 @@
   * callbacks is used to stop training on more epochs if desired value of loss or accuracy is obtained
 * CNN on Image
   * Convolution is added on top of NN in model
- 
- 
+  * Filters generated in tf.keras.layers.Conv2D are not random. They start with a set of known good filters in a similar 
+    way to the pattern fitting that you saw earlier, and the ones that work from that set are learned over time.
+  * tf.keras.layers.MaxPooling2D(2,2) implies max value out of the 4 values of 2*2 matrix survives
  
 ### Neural Networks
 # keras is a Tensorflow API
@@ -75,7 +76,7 @@ class myCallback(tf.keras.callbacks.Callback):
 model = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28, 28, 1)), #Generate 64 filters, each of 3*3 matrix, 
                                                        # ReLU activation, 1 in input shape is where we are mentioning colour depth
-  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.MaxPooling2D(2, 2),  # max value out of the 4 values of 2*2 matrix survives
   tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
   tf.keras.layers.MaxPooling2D(2,2),
   tf.keras.layers.Flatten(),
@@ -83,8 +84,9 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10, activation='softmax')
 ])
 
+model.summary()
 
-
+### 
 
 
 
