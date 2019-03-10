aa870c08cea9776c512ac4ee55722a940e8f16cc
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-03-10 16:51:17 -0500

    Update CNN.py

--- Deep Learning/CNN.py
+++ Deep Learning/CNN.py
@@ -75,21 +75,69 @@ Rescale is used to normalize data
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+# Note that because we are facing a two-class classification problem, i.e. a binary classification problem,
+# we will end our network with a sigmoid activation
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 300x300 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+# In this case, using the RMSprop optimization algorithm is preferable to stochastic gradient descent (SGD), because 
+# RMSprop automates learning-rate tuning for us. (Other optimizers, such as Adam and Adagrad, also automatically adapt
+# the learning rate during training, and would work equally well here.)
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+
 # All images will be rescaled by 1./255
 train_datagen = ImageDataGenerator(rescale=1/255)
 
 # Flow training images in batches of 128 using train_datagen generator
 train_generator = train_datagen.flow_from_directory(
-        '/tmp/horse-or-human/',  # This is the source directory for training images - Names of Subdirectories will be the labels of the images
+                 # This is the source directory for training images - Names of Subdirectories will be the labels of the images
+        'C:/Users/sandh/Google Drive/Data Science/Python Learning/DataSets/Images/Horses_Humans/Train_Horse_Human/',  
         target_size=(300, 300),  # All images will be resized to 150x150. Input data of NN should all be of same size
         batch_size=128,
-        # Since we use binary_crossentropy loss, we need binary labels
+           # Since we use binary_crossentropy loss, we need binary labels
         class_mode='binary')
 
+from tensorflow.keras.optimizers import RMSprop
 
+# fit the model
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=8,  
+      epochs=15,
+      verbose=1)
 
-
-
+# Validation Generator
+valid_generator = valid_datagen.flow_from_directory(
+                 # This is the source directory for training images - Names of Subdirectories will be the labels of the images
+        'C:/Users/sandh/Google Drive/Data Science/Python Learning/DataSets/Images/Horses_Humans/Train_Horse_Human/',  
+        target_size=(300, 300),  # All images will be resized to 150x150. Input data of NN should all be of same size
+        batch_size=128,
+           # Since we use binary_crossentropy loss, we need binary labels
+        class_mode='binary')
 
   
   
