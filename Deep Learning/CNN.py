701b4a7b3314020954f4dd595fbadbe1495ea930
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-03-10 16:53:34 -0500

    Update CNN.py

--- Deep Learning/CNN.py
+++ Deep Learning/CNN.py
@@ -80,7 +80,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 model = tf.keras.models.Sequential([
     # Note the input shape is the desired size of the image 300x300 with 3 bytes color
     # This is the first convolution
-    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)), # 3 bytes for colour, 1 for blue, 1 for green and another for red
     tf.keras.layers.MaxPooling2D(2, 2),
     # The second convolution
     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
@@ -99,8 +99,9 @@ model = tf.keras.models.Sequential([
     # 512 neuron hidden layer
     tf.keras.layers.Dense(512, activation='relu'),
     # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('horses') and 1 for the other ('humans')
-    tf.keras.layers.Dense(1, activation='sigmoid')
+    tf.keras.layers.Dense(1, activation='sigmoid') #1 neuron for Sigmoid handles binary label
 ])
+model.summary()
 
 # In this case, using the RMSprop optimization algorithm is preferable to stochastic gradient descent (SGD), because 
 # RMSprop automates learning-rate tuning for us. (Other optimizers, such as Adam and Adagrad, also automatically adapt
