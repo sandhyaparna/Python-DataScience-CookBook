94d24abfd3771e6ac75332495653785fb08e9dae
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-04-09 10:59:25 -0500

    Update Exploration.py

--- Time Series/Exploration.py
+++ Time Series/Exploration.py
@@ -2,6 +2,12 @@
 # Check for Stationarity: https://medium.com/@kangeugine/time-series-check-stationarity-1bee9085da05
 # https://www.kdnuggets.com/2018/09/end-to-end-project-time-series-analysis-forecasting-python.html
 
+### Steps 
+# Look at min and max dates for the data
+# Aggregate data to date or week or month level - Do you want to look at overall sales (sum) per day or average sales (mean) per day
+
+
+
 
 # Packages
 import warnings
@@ -33,7 +39,13 @@ var1, var2 = X1.var(), X2.var()
 print('mean1=%f, mean2=%f' % (mean1, mean2))
 print('variance1=%f, variance2=%f' % (var1, var2))
 
+### Time series decomposition into Trend, Seasonal, Residual
+from pylab import rcParams
+rcParams['figure.figsize'] = 18, 8
 
+decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+fig = decomposition.plot()
+plt.show()
 
 
 
