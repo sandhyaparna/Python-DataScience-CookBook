47407085c5559aac201a55e8aa838522274eeb89
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-06-06 13:51:44 -0500

    Update ModelDeployement_Flask.py

--- Modeling/ModelDeployement_Flask.py
+++ Modeling/ModelDeployement_Flask.py
@@ -1,11 +1,18 @@
 # https://www.kdnuggets.com/2019/01/build-api-machine-learning-model-using-flask.html
 # https://towardsdatascience.com/develop-a-nlp-model-in-python-deploy-it-with-flask-step-by-step-744f3bdd7776
+# https://medium.com/fintechexplained/flask-host-your-python-machine-learning-model-on-web-b598151886d
 # https://hackernoon.com/deploy-a-machine-learning-model-using-flask-da580f84e60c
 # https://www.analyticsvidhya.com/blog/2017/09/machine-learning-models-as-apis-using-flask/
 # https://towardsdatascience.com/designing-a-machine-learning-model-and-deploying-it-using-flask-on-heroku-9558ce6bde7b
-# 
+# https://www.analyticsindiamag.com/5-python-libraries-to-package-and-deploy-machine-learning-models/
 
-https://www.analyticsindiamag.com/5-python-libraries-to-package-and-deploy-machine-learning-models/
+### Overview ###
+# 2 key components: WSGI & Jinja2
+# First phase: Host ML model to your local machine
+# Last phase: Host ML model from local to an external web server
+
+
+
 
 
 ### Save Model as pickle file
