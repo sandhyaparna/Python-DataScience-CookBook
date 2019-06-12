2d5ff7416ba75f185e31c1a0489dcd547b8d6f28
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-06-12 10:17:24 -0500

    Update ModelDeployement.py

--- Modeling/ModelDeployement.py
+++ Modeling/ModelDeployement.py
@@ -1,3 +1,4 @@
+########## Flask ##########
 # https://www.kdnuggets.com/2019/01/build-api-machine-learning-model-using-flask.html
 # https://towardsdatascience.com/develop-a-nlp-model-in-python-deploy-it-with-flask-step-by-step-744f3bdd7776
 # https://medium.com/fintechexplained/flask-host-your-python-machine-learning-model-on-web-b598151886d
@@ -6,7 +7,7 @@
 # https://towardsdatascience.com/designing-a-machine-learning-model-and-deploying-it-using-flask-on-heroku-9558ce6bde7b
 # https://www.analyticsindiamag.com/5-python-libraries-to-package-and-deploy-machine-learning-models/
 
-### Overview ###
+### Overview 
 # 2 key components: WSGI & Jinja2
 # First phase: Host ML model to your local machine
 # Last phase: Host ML model from local to an external web server
@@ -21,5 +22,10 @@ pickle.dump(clf, open('models/final_prediction.pickle', 'wb'))
 
 # You can then open this pickle file later and call the function predict to get a prediction for new input data. 
 # This is exactly what we will do in Flask.
+
+
+
+########## Jupyter - repl GUI ##########
+
 
 
