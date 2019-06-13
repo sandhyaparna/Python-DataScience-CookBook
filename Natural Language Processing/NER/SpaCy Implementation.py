e2db58a5b20bd8ed0d3094051db35fa60a20555b
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-06-13 14:27:48 -0500

    Create SpaCy Implementation.py

---
+++ Natural Language Processing/NER/SpaCy Implementation.py
@@ -0,0 +1,39 @@
+# https://towardsdatascience.com/named-entity-recognition-with-nltk-and-spacy-8c4a7d88e7da
+
+import spacy
+from spacy import displacy
+from collections import Counter
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+
+doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
+print([(X.text, X.label_) for X in doc.ents])
+
+# Extracting Named Entities from article on web
+from bs4 import BeautifulSoup
+import requests
+import re
+def url_to_string(url):
+    res = requests.get(url)
+    html = res.text
+    soup = BeautifulSoup(html, 'html5lib')
+    for script in soup(["script", "style", 'aside']):
+        script.extract()
+    return " ".join(re.split(r'[\n\t]+', soup.get_text()))
+    
+ny_bb = url_to_string('https://www.nytimes.com/2018/08/13/us/politics/peter-strzok-fired-fbi.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=first-column-region&region=top-news&WT.nav=top-news')
+article = nlp(ny_bb)
+len(article.ents) # Number of entities in the article
+
+
+labels = [x.label_ for x in article.ents]
+Counter(labels)
+
+
+
+
+
+
+
+
+
