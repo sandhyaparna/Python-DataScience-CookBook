1059e7aebcf9d99a0f8ec9fd5baf55e06e178b59
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-07-25 09:20:15 -0500

    Update DeepLearningAICourse.py

--- Deep Learning/DeepLearningAICourse.py
+++ Deep Learning/DeepLearningAICourse.py
@@ -68,7 +68,8 @@ model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28,28))
 # for image data - Normalize the input values
 
 
-# Stops training based on callbacks
+# Stops training when I reach a point I want to be at based on callbacks. 
+# In every epoch, you can callback to a code function, having checked the metrics, if they are reached, u can cancel training at that point
 # callbacks is based on loss
 class myCallback(tf.keras.callbacks.Callback):
   def on_epoch_end(self, epoch, logs={}):
@@ -85,6 +86,26 @@ class myCallback(tf.keras.callbacks.Callback):
       print("\nReached 99% accuracy so cancelling training!")
       self.model.stop_training = True
 
+import tensorflow as tf
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc')>0.6):
+      print("\nReached 60% accuracy so cancelling training!")
+      self.model.stop_training = True
+mnist = tf.keras.datasets.fashion_mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+callbacks = myCallback()
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])      
+      
 ### CNN
 model = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28, 28, 1)), #Generate 64 filters, each of 3*3 matrix, 
