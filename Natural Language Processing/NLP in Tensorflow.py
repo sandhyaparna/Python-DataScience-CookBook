aecc297629277dd1dfc84f8d08da252752a50d8e
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-07-30 13:55:53 -0500

    Update NLP in Tensorflow.py

--- Natural Language Processing/NLP in Tensorflow.py
+++ Natural Language Processing/NLP in Tensorflow.py
@@ -2,6 +2,12 @@
 # https://github.com/lmoroney/dlaicourse/tree/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP
 # https://colab.research.google.com/drive/1znO9kRsbYRV79W0qkd4AGzTRlJexYRod
 
+## Different deep learning algos for NLP
+# CNN - Convolutional Neural Network
+# RNN - Recurrent Neural Network
+# HNN - Hierarchical Attention Network
+
+
 
 
 
