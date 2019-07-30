5325df8d1da2d9f241030845e8d42a4ba3556937
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-07-30 12:51:06 -0500

    Update NLP in Tensorflow.py

--- Natural Language Processing/NLP in Tensorflow.py
+++ Natural Language Processing/NLP in Tensorflow.py
@@ -1,6 +1,6 @@
 # https://www.coursera.org/learn/natural-language-processing-tensorflow/home/welcome
 # https://github.com/lmoroney/dlaicourse/tree/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP
-
+# https://colab.research.google.com/drive/1znO9kRsbYRV79W0qkd4AGzTRlJexYRod
 
 
 
