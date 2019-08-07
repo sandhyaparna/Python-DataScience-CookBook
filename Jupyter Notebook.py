f5a77e8b2b0c7d78578908eb13684f73aea46e27
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-08-07 11:43:09 -0500

    Update Jupyter Notebook.py

--- Jupyter Notebook.py
+++ Jupyter Notebook.py
@@ -1,7 +1,9 @@
 # Import data
 Df = pd.read_csv('path/file.csv', encoding='latin-1')
 
-
+# Description
+def return_desc(df): 
+    return print (df.dtypes),print (df.head(3)) ,print(df.apply(lambda x: [x.unique()])), print(df.apply(lambda x: [len(x.unique())])),print (df.shape)
 
 
 
