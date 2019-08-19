5300ef9f69790b2b60eb1b7224a4252eea22b75e
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-08-19 14:16:17 -0500

    Create Transfer Learning.py

---
+++ Deep Learning/Transfer Learning.py
@@ -0,0 +1,15 @@
+# https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/Course%202%20-%20Part%206%20-%20Lesson%203%20-%20Notebook.ipynb#scrollTo=C2Fp6Se9rKuL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
