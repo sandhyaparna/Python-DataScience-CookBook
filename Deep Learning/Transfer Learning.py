82f11f093ae9cc38d7bc315014d4b052e1a21bf4
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-08-19 14:55:43 -0500

    Update Transfer Learning.py

--- Deep Learning/Transfer Learning.py
+++ Deep Learning/Transfer Learning.py
@@ -1,6 +1,6 @@
 # https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/Course%202%20-%20Part%206%20-%20Lesson%203%20-%20Notebook.ipynb#scrollTo=C2Fp6Se9rKuL
-
-
+# Apply pre-trained inception model to Horse vs Humans classification model: https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/Exercises/Exercise%207%20-%20Transfer%20Learning/Exercise%207%20-%20Answer.ipynb
+# Apply pre-trained inception model to Dogs vs Cats classification model: https://colab.research.google.com/github/lmoroney/dlaicourse/blob/master/Course%202%20-%20Part%206%20-%20Lesson%203%20-%20Notebook.ipynb
 
 
 
