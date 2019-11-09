041ea8fdc4eddb271d38b0bd9dc2bdf6dbe8e57d
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2019-11-09 17:40:10 -0600

    Update Exploratory Data Analysis.py

--- Exploratory Data Analysis.py
+++ Exploratory Data Analysis.py
@@ -67,6 +67,47 @@ Df = pd.read_csv("path/file.csv",
 INPUT_DIR = '.../folder/'
 Df = pd.read_csv(INPUT_DIR + 'file.csv')
 
+# Import data as reduced file (Function)
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+# Import files
+%%time
+warnings.simplefilter('ignore')
+files = ['../input/ieee-fraud-detection/test_identity.csv', 
+         '../input/ieee-fraud-detection/test_transaction.csv',
+         '../input/ieee-fraud-detection/train_identity.csv',
+         '../input/ieee-fraud-detection/train_transaction.csv',
+         '../input/ieee-fraud-detection/sample_submission.csv']
+def load_data(file):
+    return reduce_mem_usage(pd.read_csv(file))
+with multiprocessing.Pool() as pool:
+    test_identity, test_transaction, train_identity, train_transaction, sample_submission = pool.map(load_data, files)
+
 # Import csv 2 column file as a dictionary
 feature_set = ufile.read_csv_as_dict ('data\\numeric_features.csv', 0, 1, True) #Valx - Data
 greater, greater_equal, greater_equal2, lower, lower_equal, lower_equal2, equal, between, selects, connect, features, temporal, temporal_con, error1, error2, symbols, numbers, unit_special, unit_ori, unit_ori_s, unit_exp, negation = "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
