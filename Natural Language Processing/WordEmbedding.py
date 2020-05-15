ababa5e376106aeeee94f67bb1ead495dc472ed4
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2020-05-15 13:43:35 -0500

    Update WordEmbedding.py

--- Natural Language Processing/WordEmbedding.py
+++ Natural Language Processing/WordEmbedding.py
@@ -15,7 +15,7 @@ model_ted.wv.most_similar(“man”) #Vector for Man
 ##### FastText
 from gensim.models import FastText
 model_ted = FastText(sentences_ted, size=100, window=5, min_count=5, workers=4,sg=1)
-
+model_ted.wv.most_similar("Gastroenteritis")
 
 
 
