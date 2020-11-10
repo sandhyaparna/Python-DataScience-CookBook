f7b413e187b0edaed5088a1b4f679e52d037cb9b
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2020-11-10 00:22:14 -0600

    Update IntoDeepLearningAICourse.py

--- Deep Learning/IntoDeepLearningAICourse.py
+++ Deep Learning/IntoDeepLearningAICourse.py
@@ -1,6 +1,9 @@
 ### Google Colab Notebooks
+#Go throught all code notebooks in https://github.com/sandhyaparna/dlaicourse
 # Notebooks https://github.com/lmoroney/dlaicourse/tree/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP
 https://colab.research.google.com/drive/1znO9kRsbYRV79W0qkd4AGzTRlJexYRod#scrollTo=ZyvecH2C_46Q
+ 
+ 
  
 # Week3 - CNN
 # Week4 - Image Generator
