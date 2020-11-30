b5b40726b4298ed45d912f3c06d2ca081dd1005d
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2020-11-29 20:03:18 -0600

    Update Feature Selection.py

--- Modeling/Feature Selection.py
+++ Modeling/Feature Selection.py
@@ -2,8 +2,6 @@
 # https://medium.com/@sundarstyles89/weight-of-evidence-and-information-value-using-python-6f05072e83eb
 # https://github.com/Sundar0989/WOE-and-IV/blob/master/WOE_IV.ipynb
 # https://www.analyticsvidhya.com/blog/2018/08/dimensionality-reduction-techniques-python/
-# https://www.analyticsvidhya.com/blog/2018/08/dimensionality-reduction-techniques-python/
-
 
 
 
