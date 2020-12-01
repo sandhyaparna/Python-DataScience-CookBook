a1bce00c5fcf7e0f683ea794bcbfa0d65772ade3
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2020-11-30 20:38:00 -0600

    Update Feature Selection.py

--- Modeling/Feature Selection.py
+++ Modeling/Feature Selection.py
@@ -1,4 +1,4 @@
-### Weight of evidence and Information Value using Python
+### Weight of evidence and Information Value using Python - Gives IV for both Categorical & Numeric input features when Target var is categorical
 # https://medium.com/@sundarstyles89/weight-of-evidence-and-information-value-using-python-6f05072e83eb
 # https://github.com/Sundar0989/WOE-and-IV/blob/master/WOE_IV.ipynb
 # https://www.analyticsvidhya.com/blog/2018/08/dimensionality-reduction-techniques-python/
@@ -26,10 +26,12 @@ def select_features(X_train, y_train, X_test):
 # Mutual information from the field of information theory is the application of information gain (typically used in the construction of decision trees) to feature selection.
 # Mutual information is calculated between two variables and measures the reduction in uncertainty for one variable given a known value of the other variable.
 # uses score_func=mutual_info_classif in SelectKBest
-fs = SelectKBest(score_func=mutual_info_classif, k='all')
+fs = SelectKBest(score_func=mutual_info_classif, k='all') 
 
+# Numerical input and Numerical Output variables
+fs = SelectKBest(score_func=f_regression, k=10) #k=10 gives list of top 10 variables with high correlation
 
-
-
+# ANOVA  for Numerical input and Categorical output variables
+fs = SelectKBest(score_func=f_classif, k=2)
 
 
