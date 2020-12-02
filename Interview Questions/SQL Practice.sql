79dd9c262654267c28dea96dbb25140704937c95
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2020-12-02 16:10:50 -0600

    Update SQL Practice.sql

--- Interview Questions/SQL Practice.sql
+++ Interview Questions/SQL Practice.sql
@@ -1,7 +1,7 @@
 --Missing in where statement
 where Var IS NULL
 
-# IN in where
+--IN in where
 where Var IN ('X','Y')
 
 # If a var has matching string - https://www.sqlservertutorial.net/sql-server-basics/sql-server-like/
