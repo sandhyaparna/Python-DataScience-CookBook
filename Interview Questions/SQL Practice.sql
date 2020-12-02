8d9b1742f2c09b268417a0a7bdc8e202e49801f7
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2020-12-02 16:10:33 -0600

    Create SQL Practice.sql

---
+++ Interview Questions/SQL Practice.sql
@@ -0,0 +1,26 @@
+--Missing in where statement
+where Var IS NULL
+
+# IN in where
+where Var IN ('X','Y')
+
+# If a var has matching string - https://www.sqlservertutorial.net/sql-server-basics/sql-server-like/
+where Var LIKE '%DIAB1%' #Word DIAB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
