8dc1f9bdc8a53df7f2174c695ad7d205bf2458d9
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2021-02-16 10:23:01 -0600

    Create 1. Python Packages.py

---
+++ Modeling/1. Python Packages.py
@@ -0,0 +1,15 @@
+# Pandas Profiling is another package that automatically conducts exploratory data analysis and consolidates it into a report. I find this extremely useful to use when I’m working with smaller datasets. The best part is that it requires only one line of code!
+
+# Gradio is a Python package that allows you to build and deploy a web app for your machine learning model in as little as three lines of code. It serves the same purpose as Streamlit or Flask, but I found it much faster and easier to get a model deployed.
+
+# Kedro is a development workflow tool that allows you to create portable ML pipelines. It applies software engineering best practices to your code, making it reproducible, modular, and well-documented.
+
+
+
+
+
+
+
+
+
+
