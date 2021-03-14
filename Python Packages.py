802de5c7d2c8837af376e71aee1a5d200d7b62c5
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2021-03-13 22:33:20 -0600

    Update Python Packages.py

--- Python Packages.py
+++ Python Packages.py
@@ -7,8 +7,17 @@ Streamlit or Flask, but I found it much faster and easier to get a model deploye
 """ Kedro is a development workflow tool that allows you to create portable ML pipelines. It applies software engineering best practices to your code, making it reproducible, 
 modular, and well-documented.""" 
 
+https://link.medium.com/NjZdx2P8Xdb
 
- https://link.medium.com/NjZdx2P8Xdb
+""" CLIP: Contrastive Language-Image Pre-training
+*  You can input an image into the CLIP model, and it will return for you the likeliest caption or summary of that image
+""" 
+
+
+
+
+
+
 
 
 
