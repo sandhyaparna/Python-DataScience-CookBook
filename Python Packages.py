fffcb3e84291e131fc6c8603ebee75d607a3b9b9
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2021-03-14 00:58:28 -0600

    Update Python Packages.py

--- Python Packages.py
+++ Python Packages.py
@@ -9,8 +9,21 @@ modular, and well-documented."""
 
 https://link.medium.com/NjZdx2P8Xdb
 
-""" CLIP: Contrastive Language-Image Pre-training
-*  You can input an image into the CLIP model, and it will return for you the likeliest caption or summary of that image
+""" CLIP: Contrastive Language-Image Pre-training https://www.kdnuggets.com/2021/03/beginners-guide-clip-model.html
+* You can input an image into the CLIP model, and it will return for you the likeliest caption or summary of that image
+* "Zero-shot learning" is when a model attempts to predict a class it saw zero times in the training data. 
+* CLIP model is:
+  * a neural network model built on hundreds of millions of images and captions,
+  * can return the best caption given an image, and
+  * has impressive "zero-shot" capabilities, making it able to accurately predict entire classes it's never seen before!
+* The CLIP model consists of two sub-models called encoders:
+  * a text encoder that will embed (smash) text into mathematical space.
+  * an image encoder that will embed (smash) images into mathematical space.
+* Output from text encoder & image encoder i.e embedded representations from both encoders are compared using cosine similarity. </br>
+If cosine similarity between 2 encoders is high implies the model is good
+* Links
+  * https://blog.roboflow.com/how-to-use-openai-clip/
+  * https://colab.research.google.com/drive/1LXla2q9MCRRI_kTjpvag2Vz-7EGLnki5#scrollTo=lOF3Feb7jrnu
 """ 
 
 
